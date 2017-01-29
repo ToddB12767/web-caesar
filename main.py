06ecb60d90cd1f420c06dc275087c6783918b18e
Todd Bradley <todd.bradley@yahoo.com>
2017-01-29 17:25:44 -0600

    Wrapped encrypted message in a text box and added a input button

--- main.py
+++ main.py
@@ -2,18 +2,6 @@
 #
 # Copyright 2007 Google Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import caesar
 
@@ -21,7 +9,10 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = "HELLOOOOO World?!"
         encrpyted_message = caesar.encrypt(message, 13)
-        self.response.write(encrpyted_message)
+        textarea = "<textarea>" + encrpyted_message + "</textarea>"
+        submit = "<input type ='submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
